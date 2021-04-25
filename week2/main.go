329f545ce09ba2fbe908b61211737243149997fe
石亚涛 <shiyatao@qingting.fm>
2021-04-25 19:05:16 +0800

    fix

--- week2/main.go
+++ week2/main.go
@@ -1,18 +1,27 @@
 package main
 
-import "errors"
+import (
+	"context"
+	"errors"
+)
 
  var (
 	 NoRowsFoundError     = add(1060006, "没有找到任何记录")
 
  )
 //没有记录时需要抛出错误以及相应错误码，与一般SQL错误区分出来；大概逻辑如下
-func SelectUserByID()  {
-	currentSql := d.MySQL.Table(ctx, copyright.TableName()).Where(query)
-	err = currentSql.Find(&res).Error
+
+func (d *Dao) GetUserInfoByUserID(ctx context.Context, userID string) (row *entity.UserInfoSimple, err error) {
+	row = new(entity.UserInfoSimple)
+	err = d.MySQL.Table(ctx, "user_info").
+		Where(" podcast_id  =  ? ", userID).
+		Find(&row).
+		Error
 	if err == gorm.ErrRecordNotFound {
 		return nil, errors.Wrapf(errcode.NoRowsFoundError, "%s", err)
 	} else if err != nil {
 		return nil, errors.Wrapf(errcode.MysqlError, "%s", err)
 	}
+
+	return row, nil
 }
